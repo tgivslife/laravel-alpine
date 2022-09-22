a6d9f0c180d280c884f64495622f88c26fc44c9f
Teodor Ion <teodor.g.ion@gmail.com>
2022-09-22 08:48:31 +0300

    Fix crontab laravel artisan schedule:run command not working when run by php-fpm for older versions

--- 7.4/conf/docker-entrypoint.d/5-configure-laravel-scheduler.sh
+++ 7.4/conf/docker-entrypoint.d/5-configure-laravel-scheduler.sh
@@ -7,7 +7,7 @@
 function laravel_scheduler() {
   # Enable Laravel SCHEDULER
   if [[ "${LARAVEL_SCHEDULER_ENABLE}" == "1" ]]; then
-    echo "* * * * * php-fpm /var/www/html/artisan schedule:run >> /dev/null 2>&1" >>/etc/crontabs/www-data
+    echo "* * * * * php /var/www/html/artisan schedule:run >> /dev/null 2>&1" >>/etc/crontabs/www-data
   else
     rm -f /etc/supervisor.d/cron.ini
   fi
