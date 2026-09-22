aa356e61ab972680a58e68db37435c01089e28b8
Teodor Ion <teodor.g.ion@gmail.com>
2026-09-22 09:16:19 +0300

    Anchor entrypoint sed patterns for php 8.5 images
    - fix max_memory_limit being overwritten with the memory_limit value (new in PHP 8.5), which made any ini_set('memory_limit', ...) above 128M fail
    - anchor php.ini, opcache and php-fpm patterns to line start
    - anchor nginx patterns allowing and preserving leading indentation
    - applies to 8.5 alpine and trixie images

--- 8.5/conf/docker-entrypoint.d/4-configure-nginx.sh
+++ 8.5/conf/docker-entrypoint.d/4-configure-nginx.sh
@@ -17,11 +17,11 @@ log() {
 
 nginx() {
   NGINX_CONF_FILE="/etc/nginx/nginx.conf"
-  sed -i -e 's|set_real_ip_from .*$|set_real_ip_from '"${NGINX_SET_REAL_IP_FROM}"';|g' "$NGINX_CONF_FILE"
-  sed -i -e 's|worker_rlimit_nofile .*$|worker_rlimit_nofile '"${NGINX_WORKER_RLIMIT_NOFILE}"';|g' "$NGINX_CONF_FILE"
-  sed -i -e 's|worker_connections .*$|worker_connections '"${NGINX_WORKER_CONNECTIONS}"';|g' "$NGINX_CONF_FILE"
-  sed -i -e 's|fastcgi_read_timeout .*$|fastcgi_read_timeout '"${NGINX_FASTCGI_READ_TIMEOUT}"';|g' "$NGINX_CONF_FILE"
-  sed -i -e 's|client_max_body_size .*$|client_max_body_size '"${NGINX_CLIENT_MAX_BODY_SIZE}"';|g' "$NGINX_CONF_FILE"
+  sed -i -e 's|^\([[:space:]]*\)set_real_ip_from .*$|\1set_real_ip_from '"${NGINX_SET_REAL_IP_FROM}"';|g' "$NGINX_CONF_FILE"
+  sed -i -e 's|^\([[:space:]]*\)worker_rlimit_nofile .*$|\1worker_rlimit_nofile '"${NGINX_WORKER_RLIMIT_NOFILE}"';|g' "$NGINX_CONF_FILE"
+  sed -i -e 's|^\([[:space:]]*\)worker_connections .*$|\1worker_connections '"${NGINX_WORKER_CONNECTIONS}"';|g' "$NGINX_CONF_FILE"
+  sed -i -e 's|^\([[:space:]]*\)fastcgi_read_timeout .*$|\1fastcgi_read_timeout '"${NGINX_FASTCGI_READ_TIMEOUT}"';|g' "$NGINX_CONF_FILE"
+  sed -i -e 's|^\([[:space:]]*\)client_max_body_size .*$|\1client_max_body_size '"${NGINX_CLIENT_MAX_BODY_SIZE}"';|g' "$NGINX_CONF_FILE"
 }
 
 log "Configure nginx started"
